5f8a17b783f1577d3a6299b9082dd4f5a1b8f877
AntonKralin <kralin.dart@gmail.com>
2026-01-12 14:48:02 +0300

    test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FastApi_test
 fast api testing
+
+# Install
+sudo apt-get install libpq-dev python3-dev
---
+++ src/core/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker,
+)
+
+from core.config import database_config, project_config
+
+
+engine = create_async_engine(
+    database_config.get_url(),
+    echo=project_config.debug,
+    pool_size=database_config.db_pool_size,
+    max_overflow=database_config.db_max_overflow,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+
+async def get_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
